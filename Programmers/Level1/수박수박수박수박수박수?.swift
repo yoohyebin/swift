22a4937acbf36edf466802e8061c4b38c1175bff
hyebin-yoo <hyebin218@naver.com>
2022-04-14 14:09:56 +0900

    수박수박수박수박수박수?

---
+++ Programmers/Level1/수박수박수박수박수박수?.swift
@@ -0,0 +1,13 @@
+/*
+  길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+*/
+
+//Solution 1
+func solution(_ n:Int) -> String {
+    return "\(String(repeating: "수박", count: n / 2))\(n % 2 == 0 ? "" : "수")"
+}
+
+//Solution 2
+func solution(_ n:Int) -> String {
+    return "\(String(repeating: "수박", count: n / 2))\(n % 2 == 0 ? "" : "수")"
+}
