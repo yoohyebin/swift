309ec67d424ab4b0a63ac9eef9e978a67029e304
hyebin-yoo <hyebin218@naver.com>
2022-06-09 14:47:40 +0900

    만들 수 없는 금액

--- 이것이_코딩_테스트다/그리디/만들_수_없는_금액.swift
+++ 이것이_코딩_테스트다/그리디/만들_수_없는_금액.swift
@@ -2,3 +2,17 @@
 동네 편의점의 주인인 동빈이는 N개의 동전을 가지고 있습니다.
 이때 N개의 동전을 이용하여 만들 수 없는 양의 정수 금액 중 최솟값을 구하는 프로그램을 작성하세요.
 */
+
+var n = Int(readLine()!)!
+var coins = readLine()!.split(separator: " ").map({Int(String($0))!})
+coins = coins.sorted(by: <)
+
+var target = 1
+for coin in coins{
+    if target < coin{
+        break
+    }
+    target += coin
+}
+
+print(target)
