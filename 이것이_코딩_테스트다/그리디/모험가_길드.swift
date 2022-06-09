f5b476d8ffecca4c6c9feea7921fcabd587d5102
hyebin-yoo <hyebin218@naver.com>
2022-06-09 14:32:48 +0900

    모험가 길드

--- 이것이_코딩_테스트다/그리디/모험가_길드.swift
+++ 이것이_코딩_테스트다/그리디/모험가_길드.swift
@@ -4,14 +4,16 @@
   모험가 길드장은 모험가 그룹을 안전하게 구성하고자 공포도가 X인 모험가는 반드시 X명 이상으로 구성한 모험가 그룹에 참여해야한다고 규정했습니다.
 
   N명의 모험가에 대한 정보가 주어졌을 때, 여행을 떠날 수 있는 그룹 수의 최댓값을 구하는 프로그램을 작성하시오.
+
+  => 공포도가 낮은 순으로 정렬한 후, 해당 그룹에 포함된 사람이 공포도 보다 높거나 같다면 다음 그룹으로 옮겨 모험가를 추가한다.
 */
 
 var n = Int(readLine()!)!
 var people = readLine()!.split(separator: " ").map{Int(String($0))!}
 people = people.sorted(by: <)
 
-var cnt = 0
-var re = 0
+var cnt = 0 //그룹에 포함된 모험가 수
+var re = 0 // 그룹 수
 
 for p in people{
     cnt += 1
