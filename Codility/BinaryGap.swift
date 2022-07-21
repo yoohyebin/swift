0796b87886830da9d2dcac074f65bf7c231968f3
hyebin-yoo <hyebin218@naver.com>
2022-07-21 15:07:55 +0900

    Create BinaryGap.swift

---
+++ Codility/BinaryGap.swift
@@ -0,0 +1,14 @@
+public func solution(_ N : Int) -> Int {
+    let binary = Array(String(N, radix: 2))
+    var zero_cnt = 0, re = 0
+
+    for i in 1..<binary.count{
+        if binary[i] == "0"{
+            zero_cnt += 1
+        }else{
+            re = max(re, zero_cnt)
+            zero_cnt = 0
+        }
+    }
+    return re+1 == binary.count ? 0 : re
+}
