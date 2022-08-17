ad836c481903e139f98a7febee27a946ed33edc0
hyebin-yoo <hyebin218@naver.com>
2022-08-17 18:56:36 +0900

    Create baekjoon_11722.swift

---
+++ baekjoon/baekjoon_11722.swift
@@ -0,0 +1,13 @@
+let n = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map{Int($0)!}
+var dp = Array(repeating: 1, count: n)
+
+for i in 1..<n{
+    for j in 0..<i{
+        if (arr[i]<arr[j]) && (dp[i] < dp[j]+1){
+            dp[i] = dp[j]+1
+        }
+    }
+}
+
+print(dp.max()!)
