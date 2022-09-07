a497f66d68224ea2631463ac356b497c8674f320
hyebin-yoo <hyebin218@naver.com>
2022-09-07 13:14:05 +0900

    Create baekjoon_14501.swift

---
+++ baekjoon/baekjoon_14501.swift
@@ -0,0 +1,17 @@
+var N = Int(readLine()!)!
+var tp = [[Int]]()
+var dp = Array(repeating: 0, count: 100)
+
+for i in 0..<N{
+    tp.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+for i in 0..<N{
+    if dp[i] > dp[i+1]{
+        dp[i+1] = dp[i]
+    }
+    if dp[i+tp[i][0]] < dp[i] + tp[i][1]{
+        dp[i+tp[i][0]] = dp[i] + tp[i][1]
+    }
+}
+print(dp[N])
