07b4eef688ce6e1f625b3349335c9410e2543862
hyebin-yoo <hyebin218@naver.com>
2022-11-25 13:29:23 +0900

    Update 배열_원소의_길이.swift

--- Programmers/Level0/배열_원소의_길이.swift
+++ Programmers/Level0/배열_원소의_길이.swift
@@ -1,9 +1,15 @@
 import Foundation
 
+//Solution 1
 func solution(_ strlist:[String]) -> [Int] {
     var answer = [Int]()
     for str in strlist{
         answer.append(str.count)
     }
     return answer
+}
+
+//Solution 2
+func solution(_ strlist:[String]) -> [Int] {
+    return strlist.map({ $0.count })
 }
