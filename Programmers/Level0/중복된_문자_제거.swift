071e818dfc4ebbdd7cd267881b262f01cffbf6ec
hyebin-yoo <hyebin218@naver.com>
2022-12-26 15:22:02 +0900

    Update 중복된_문자_제거.swift

--- Programmers/Level0/중복된_문자_제거.swift
+++ Programmers/Level0/중복된_문자_제거.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    var answer = [String]()
-    for s in my_string.map{String($0)}{
-        if !answer.contains(s) {answer.append(s)}
+    var result = ""
+    
+    for c in my_string {
+        if !result.contains(c) {
+            result += String(c)
+        }
     }
-    return answer.joined()
+    return result
 }
