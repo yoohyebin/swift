2a4a32709f6a3a71b4a53bd9ad3aafa824467920
hyebin-yoo <hyebin218@naver.com>
2023-01-31 13:45:12 +0900

    Update 모의고사.swift

--- Programmers/Level1/모의고사.swift
+++ Programmers/Level1/모의고사.swift
@@ -10,41 +10,18 @@
 
 import Foundation
 
-//Solution1
 func solution(_ answers:[Int]) -> [Int] {
-    var s1 = [1,2,3,4,5]
-    var s2 = [2,1,2,3,2,4,2,5]
-    var s3 = [3,3,1,1,2,2,4,4,5,5]
-    var score = [0,0,0]
+    let s1 = [1,2,3,4,5]
+    let s2 = [2,1,2,3,2,4,2,5]
+    let s3 = [3,3,1,1,2,2,4,4,5,5]
+    var score = [1:0, 2:0, 3:0]
     
     for i in 0..<answers.count{
-        if(answers[i] == s1[i%5]){score[0]+=1}
-        if(answers[i] == s2[i%8]){score[1]+=1}
-        if(answers[i] == s3[i%10]){score[2]+=1}
+        if(answers[i] == s1[i%5]){score[1]! += 1}
+        if(answers[i] == s2[i%8]){score[2]! += 1}
+        if(answers[i] == s3[i%10]){score[3]! += 1}
     }
-    var max = score.max()!
-    var a = [Int]()
     
-    if score[0] == max {a.append(1)}
-    if score[1] == max {a.append(2)}
-    if score[2] == max {a.append(3)}
-    
-    return a
-}
-//Solution2
-func solution(_ answers:[Int]) -> [Int] {
-    let answer = (
-        a: [1, 2, 3, 4, 5], // index % 5
-        b: [2, 1, 2, 3, 2, 4, 2, 5], // index % 8
-        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // index % 10
-    )
-    var point = [1:0, 2:0, 3:0]
-
-    for (i, v) in answers.enumerated() {
-        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
-        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
-        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
-    }
-
-    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+    let max = score.values.max()
+    return score.filter{$0.value == max}.keys.sorted(by: <)
 }
