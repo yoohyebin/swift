bf40857ee1debb7d673fb3b3f3711d2add1be559
hyebin-yoo <hyebin218@naver.com>
2023-02-21 12:36:59 +0900

    Create 카드뭉치.swift

---
+++ Programmers/Level1/카드뭉치.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
+    var cards1 = cards1, cards2 = cards2
+
+    for s in goal{
+        if !cards1.isEmpty && cards1[0] == s {
+            cards1.removeFirst()
+        }else if !cards2.isEmpty && cards2[0] == s {
+            cards2.removeFirst()
+        }else{
+            return "No"
+        }
+    }
+    return "Yes"
+}
