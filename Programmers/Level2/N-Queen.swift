8d3c823b68c74dee6f24755ede522fb5512f29cd
hyebin-yoo <hyebin218@naver.com>
2023-02-23 23:32:23 +0900

    Create N-Queen.swift

---
+++ Programmers/Level2/N-Queen.swift
@@ -0,0 +1,31 @@
+func solution(_ n:Int) -> Int {
+    var chess: [Int] = Aarray(repeating: -1, count: n)
+    var answer = 0
+
+    func check(_ x: Int, _ y: Int) -> Bool {
+        for i in 0..<x {
+            if chess[i] == y || abs(x - i) - abs(y - chess[i]) == 0 { return false }
+        }
+
+        return true
+    }
+
+    func dfs(_ cnt: Int) {
+        if cnt == n {
+            answer += 1
+            return
+        }
+
+        for i in 0..<n {
+            if check(cnt, i) {
+                chess[cnt] = i
+                dfs(cnt + 1)
+                chess[cnt] = -1
+            }
+        }
+    }
+
+    dfs(0)
+
+    return answer
+}
