9726ddfc9351bb9d64ecc5309c69a0ae58958c2c
hyebin-yoo <hyebin218@naver.com>
2023-03-16 06:55:15 +0900

    Create 숫자_변환하기.swift

---
+++ Programmers/Level2/숫자_변환하기.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
+    var dp = Array(repeating: Int.max, count: y+1)
+    dp[x] = 0
+    
+    for i in x...y {
+        if dp[i] == Int.max {
+            continue
+        }
+        
+        if i+n <= y {
+            dp[i+n] = min(dp[i+n], dp[i]+1)
+        }
+        if i*2 <= y {
+            dp[i*2] = min(dp[i*2], dp[i]+1)
+        }
+        if i*3 <= y {
+            dp[i*3] = min(dp[i*3], dp[i]+1)
+        }
+    }
+    
+    return dp[y] == Int.max ? -1 : dp[y]
+}
