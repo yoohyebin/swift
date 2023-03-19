7e00a90c17a667a175813caab493fb4898d1c305
Hyebin (Helia) <hyebin218@naver.com>
2023-03-19 13:08:47 +0900

    Update 대충_만든_자판_도움말.swift

--- Programmers/Level1/대충_만든_자판_도움말.swift
+++ Programmers/Level1/대충_만든_자판_도움말.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-//Solution 1
 func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
     var dict = [String: Int]()
     var keymap = keymap.map{$0.map{String($0)}}
@@ -20,35 +19,14 @@ func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
         var sum = 0
         for t in target.map{String($0)} {
             if dict[t] == nil {
+                sum = -1
                 break
             }else{
                 sum += dict[t]!+1
             }
         }
-        result.append(sum == 0 ? -1 : sum)
+        result.append(sum)
     }
     
-    return result
-}
-
-//Solution 2
-func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
-    var keymap = keymap.map{$0.map{String($0)}}
-    var result = [Int]()
-    var sum = 0
-
-    for target in targets {
-        sum = 0
-        for t in target.map{String($0)} {
-            var minValue = Int.max
-            for k in keymap{
-                guard let i = k.firstIndex(of: t) else {continue}
-                minValue = min(minValue, i+1)
-            }
-            sum = minValue == Int.max ? 0 : sum+minValue
-        }
-        result.append(sum == 0 ? -1 : sum)
-    }
-
     return result
 }
